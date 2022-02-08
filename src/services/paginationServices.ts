0271e3b861dd9f79e54cb3241d114dcd1b908cd9
Pedro Henrique Spelta <80366307+PedroSpelta@users.noreply.github.com>
2022-02-08 12:11:15 -0300

    Finished getPages function

--- src/services/paginationServices.ts
+++ src/services/paginationServices.ts
@@ -1,3 +1,5 @@
+const { abs } = Math;
+
 const makeArray = (from: number, to: number, selected: number) => {
   const array = [];
   for (let i = from; i <= to; i += 1) {
@@ -12,18 +14,50 @@ const getPages = (actualPage: number, totalPages: number) => {
   if (totalPages <= 5) {
     return makeArray(1, totalPages, actualPage);
   } else {
-    const flActual = actualPage - 2;
-    const startingNumber = flActual > 1 ? flActual : 1;
-    const frActual = actualPage + 2;
-    const endingNumber = frActual < totalPages ? frActual : totalPages;
-    console.log(startingNumber, endingNumber, actualPage);
-    
-    return makeArray(startingNumber, endingNumber, actualPage);
+    let first = 0,
+      last = 0;
+    const leftNumbers = abs(actualPage - 1) < 4 ? abs(actualPage - 1) : 4;
+    const rightNumbers = totalPages - actualPage < 4 ? totalPages - actualPage : 4;
+    // const minNumber = flActual > 1 ? flActual : 1;
+    // const maxNumber = frActual < totalPages ? frActual : totalPages;
+    const twoBigNumbers = leftNumbers > 2 && rightNumbers > 2;
+
+    if (leftNumbers === rightNumbers || twoBigNumbers) {
+      if (leftNumbers > 2) {
+        first = actualPage - 2;
+        last = actualPage + 2;
+      } else {
+        first = actualPage - leftNumbers;
+        last = actualPage + rightNumbers;
+      }
+    } else if (leftNumbers > rightNumbers) {
+      first = actualPage - (leftNumbers - rightNumbers);
+      last = actualPage + rightNumbers;
+    } else if (leftNumbers < rightNumbers) {
+      first = actualPage - leftNumbers;
+      last = actualPage + (rightNumbers - leftNumbers);
+    }
+
+    return makeArray(first, last, actualPage);
   }
 };
 
+const getContinuity = (pages: Array<string>, totalPages: number) => {
+  const withContinuity: Array<string> = [];
+  if (pages[0] !== '1' && pages[0] !== '**1**') withContinuity.push('...');
+  withContinuity.push(...pages);
+  if (
+    pages[pages.length - 1] !== totalPages.toString() &&
+    pages[pages.length - 1] !== `**${totalPages.toString()}**`
+  )
+    withContinuity.push('...');
+  return withContinuity;
+};
+
 const getPagination = async (actualPage: any, totalPages: any) => {
-  return getPages(actualPage, totalPages);
+  const pages = getPages(actualPage, totalPages);
+  const pagination = getContinuity(pages, totalPages);
+  return pagination;
 };
 
 export default {
